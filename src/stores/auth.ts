169211976eb61d1018f8f2c618c635df9b8e307a
LeandroRochAlg <leandrochaju@hotmail.com>
2024-06-21 15:21:38 -0300

    feat: Add logout functionality

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia";
-import { auth } from "../firebaseConfig";
+import { auth } from "../libs/firebaseConfig";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import api from '../services/api';
 import { UserRegister } from "../types/UserRegister";
@@ -7,7 +7,8 @@ import { AxiosError } from "axios";
 
 export const useAuthStore = defineStore('auth', {
     state: () => ({
-        user: null as any | null,
+        user: localStorage.getItem('user') || null,
+        googleUser: null as any | null,
         idGoogle: localStorage.getItem('idGoogle') || null,
         isAuthenticated: localStorage.getItem('idGoogle') !== null,
     }),
@@ -17,7 +18,7 @@ export const useAuthStore = defineStore('auth', {
 
             try {
                 const result = await signInWithPopup(auth, provider);
-                this.user = result.user;
+                this.googleUser = result.user;
                 this.idGoogle = result.user.uid;
                 this.isAuthenticated = true;
 
@@ -53,11 +54,24 @@ export const useAuthStore = defineStore('auth', {
                 };
 
                 const response = await api.login(user);
+
                 console.log('User logged in', response.data);
+
+                // Redirect to the home page
+                location.href = '/home';
             } catch (error) {
                 const axiosError = error as AxiosError;
                 console.error(axiosError.response?.data);
             }
         },
+        async logout() {
+            try {
+                await api.logout();
+
+                location.href = '/login'; // Redirect to the login page
+            } catch (error) {
+                console.error('Error during logout', error);
+            }
+        }
     }
 })
