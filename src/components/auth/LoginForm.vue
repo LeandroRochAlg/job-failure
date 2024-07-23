de3938d02fb5ed76fd9662d710737487921d462b
LeandroRochAlg <leandrochaju@hotmail.com>
2024-07-22 21:45:11 -0300

    refactor: Improve login form styling and structure

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,10 +1,10 @@
 <template>
-    <div>
-        <h2>Login</h2>
-        <form @submit.prevent="login">
-            <input v-model="username" placeholder="Username" required>
-            <input v-model="password" type="password" placeholder="Password" required>
-            <button type="submit">Login</button>
+    <div class="w-1/4 h-full mx-auto font-montserrat flex flex-col align-middle justify-center">
+        <h2 class="text-primary text-6xl mb-5">Login</h2>
+        <form @submit.prevent="login" class="flex flex-col">
+            <input v-model="username" placeholder="Username" required class="focus:outline-none focus:underline hover:underline mt-2 border-none bg-darkLight rounded-t-xl">
+            <input v-model="password" type="password" placeholder="Password" required class="focus:outline-none focus:underline hover:underline mt-1 border-none bg-darkLight rounded-b-xl">
+            <button type="submit" class="h-10 bg-primary rounded-full w-1/2 text-light uppercase font-bold mx-auto mt-5">Login</button>
         </form>
     </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      primary: '#600460',
+      dark: '#2B2A2C',
+      light: '#F1EFF5',
+      lightDark: '#464547',
+      darkLight: '#DAD8E0'
+    },
+    fontFamily: {
+      montserrat: ['Montserrat', 'sans-serif'],
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
