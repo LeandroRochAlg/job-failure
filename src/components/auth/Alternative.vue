6e38baeed7aa4f5cc03ce09e77036019ea227b58
LeandroRochAlg <leandrochaju@hotmail.com>
2024-07-23 17:38:09 -0300

    chore: Update vue-tsc dependency to version 1.6.5 and add Alternative component for registration link in LoginForm.vue

---
+++ src/components/auth/Alternative.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="text-center text-sm italic my-6">
+        <p class="font-light">don't have an account yet?</p>
+        <p class="font-bold hover:underline"><a href="/register">create one now</a></p>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-[400px] h-[500px] mx-auto font-montserrat flex flex-col align-middle bg-light rounded-tr-xl rounded-bl-xl shadow-xl">
+    <div class="w-[400px] h-[500px] mx-auto font-montserrat flex flex-col align-middle bg-light rounded-tr-xl rounded-bl-xl shadow-xl justify-between">
         <form @submit.prevent="login" class="flex flex-col h-full justify-between w-full">
             <Title title="Login"/>
             <div class="w-full flex flex-col">
@@ -8,6 +8,15 @@
             </div>
             <Button title="Login"/>
         </form>
+        <div class="w-full flex items-center justify-center my-6">
+            <div class="flex-grow border-t border-dark"></div>
+            <span class="mx-2 text-dark text-sm">or login with</span>
+            <div class="flex-grow border-t border-dark"></div>
+        </div>
+        <button class="flex mx-auto hover:bg-darkLight p-2 rounded-xl">
+            <img :src="googleLogo" alt="Google Logo" class="w-[30px]">
+        </button>
+        <Alternative/>
     </div>
 </template>
 
@@ -17,6 +26,8 @@ import { useAuthStore } from '../../stores/auth';
 import Button from './Button.vue';
 import Input from './Input.vue';
 import Title from './Title.vue';
+import Alternative from './Alternative.vue';
+import googleLogo from '../../assets/Google.png';
 
 const authStore = useAuthStore();
 const username = ref('');
