90826fbea8cd7ee5a9dbd1f69e962c85fc7d3eaf
LeandroRochAlg <leandrochaju@hotmail.com>
2024-07-23 17:39:31 -0300

    style: Update Alternative component registration link styling

--- src/components/auth/Alternative.vue
+++ src/components/auth/Alternative.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="text-center text-sm italic my-6">
+    <div class="text-center text-sm italic my-6 text-dark">
         <p class="font-light">don't have an account yet?</p>
         <p class="font-bold hover:underline"><a href="/register">create one now</a></p>
     </div>
