b7a4561858bf6120c645c9ddc1120f0ab2634f57
LeandroRochAlg <leandrochaju@hotmail.com>
2024-07-24 15:47:56 -0300

    refactor: Update LoginForm.vue and LoginView.vue components

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,24 +1,13 @@
 <template>
-    <div class="w-[300px] sm:w-[400px] h-[500px] mx-auto font-montserrat flex flex-col align-middle bg-light dark:bg-dark rounded-tr-xl rounded-bl-xl shadow-xl justify-between">
-        <form @submit.prevent="login" class="flex flex-col h-full justify-between w-full">
-            <Title title="Login"/>
-            <div class="w-full flex flex-col">
-                <Input type="text" v-model="username" placeHolder="Username" position="top"/>
-                <Input type="password" v-model="password" placeHolder="Password" position="bottom"/>
-            </div>
-            <Button :title="'Login'" :loading="loading"/>
-            <p v-if="!response" class="text-center text-red">Ops... You can't go there 👉</p>
-        </form>
-        <div class="w-full flex items-center justify-center my-6">
-            <div class="flex-grow border-t border-dark dark:border-light"></div>
-            <span class="mx-2 text-dark text-sm dark:text-light">or login with</span>
-            <div class="flex-grow border-t border-dark dark:border-light"></div>
+    <form @submit.prevent="login" class="flex flex-col h-full justify-between w-full">
+        <Title title="Login"/>
+        <div class="w-full flex flex-col">
+            <Input type="text" v-model="username" placeHolder="Username" position="top"/>
+            <Input type="password" v-model="password" placeHolder="Password" position="bottom"/>
         </div>
-        <button class="flex mx-auto hover:bg-darkLight dark:hover:bg-lightDark p-2 rounded-xl">
-            <img :src="googleLogo" alt="Google Logo" class="w-[30px]">
-        </button>
-        <Alternative route='/register'/>
-    </div>
+        <Button :title="'Login'" :loading="loading"/>
+        <p v-if="!response" class="text-center text-red">Ops... You can't go there 👉</p>
+    </form>
 </template>
 
 <script lang="ts" setup>
@@ -27,8 +16,6 @@ import { useAuthStore } from '../../stores/auth';
 import Button from './Button.vue';
 import Input from './Input.vue';
 import Title from './Title.vue';
-import Alternative from './Alternative.vue';
-import googleLogo from '../../assets/Google.png';
 
 const authStore = useAuthStore();
 const username = ref('');
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,17 +1,21 @@
 <template>
-    <div>
-        <LoginForm />
-    </div>
+    <Card>
+        <LoginForm/>
+        <div class="w-full flex items-center justify-center my-6">
+            <div class="flex-grow border-t border-dark dark:border-light"></div>
+            <span class="mx-2 text-dark text-sm dark:text-light">or login with</span>
+            <div class="flex-grow border-t border-dark dark:border-light"></div>
+        </div>
+        <button class="flex mx-auto hover:bg-darkLight dark:hover:bg-lightDark p-2 rounded-xl">
+            <img :src="googleLogo" alt="Google Logo" class="w-[30px]">
+        </button>
+        <Alternative route='/register'/>
+    </Card>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script lang="ts" setup>
 import LoginForm from '../components/auth/LoginForm.vue';
-
-export default defineComponent({
-    name: 'LoginView',
-    components: {
-        LoginForm,
-    }
-});
+import Card from '../components/auth/Card.vue';
+import Alternative from '../components/auth/Alternative.vue';
+import googleLogo from '../assets/Google.png';
 </script>
