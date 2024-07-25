97ff8c1a616ff2ae858bd4ac7bb1f4cc6ca1bb36
LeandroRochAlg <leandrochaju@hotmail.com>
2024-07-25 12:44:51 -0300

    refactor: Add rounded-lg class to GoogleUser.vue profile picture

--- src/components/auth/GoogleUser.vue
+++ src/components/auth/GoogleUser.vue
@@ -1,6 +1,6 @@
 <template>
     <button class="w-[275px] sm:w-[350px] mx-auto flex flex-row text-dark dark:text-light h-fit rounded-lg hover:bg-darkLight dark:hover:bg-lightDark p-2" @click="handleGoogleLogout">
-        <img :src="profilePic" alt="User's Google Account profile picture" class="w-[50px]">
+        <img :src="profilePic" alt="User's Google Account profile picture" class="w-[50px] rounded-lg">
         <div class="text-dark dark:text-light text-xs flex flex-col justify-center align-middle text-left ml-2 my-auto">
             <p><span class="font-bold">{{ props.username }}</span> connected!</p>
             <p>Connect with another account?</p>
