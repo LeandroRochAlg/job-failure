b20518361f195b9b9fd92ef610b506128d03f6cb
Leandro Rocha <leandrochaju@hotmail.com>
2025-03-04 21:03:12 -0300

    fix: Adjust minimum height of Card component for better layout

--- src/components/auth/Card.vue
+++ src/components/auth/Card.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-[300px] sm:w-[400px] h-[500px] mx-auto font-montserrat flex flex-col align-middle bg-light dark:bg-dark rounded-tr-xl rounded-bl-xl shadow-xl justify-between">
+    <div class="w-[300px] sm:w-[400px] min-h-[500px] mx-auto font-montserrat flex flex-col align-middle bg-light dark:bg-dark rounded-tr-xl rounded-bl-xl shadow-xl justify-between">
         <slot></slot>
     </div>
 </template>
